531225597a88c664781c3d180a8f9547824e2a5d
ddrofreserr <ddrofreserr@gmail.com>
2025-05-27 15:09:39 +0300

    working on data generation

--- data/create_data.py
+++ data/create_data.py
@@ -4,9 +4,8 @@ import numpy as np
 
 def generate_synthetic_data(n_samples: int = 1000) -> pd.DataFrame:
     return pd.DataFrame({
-        "sensor_1": np.random.rand(n_samples),
-        
-        "sensor_2": np.random.rand(n_samples),
-        "label": np.random.randint(0, 2, n_samples)
+        "temperature": np.random.rand(n_samples),
+        "humidity": np.random.rand(n_samples),
+        "target": np.random.randint(0, 2, n_samples)
     })
-    # тут точно добавить шумы
+    # добавить шумы
