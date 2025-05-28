f044625ffd18dc9dbf4381cb95e15e6313a79b5b
ddrofreserr <ddrofreserr@gmail.com>
2025-05-28 22:15:54 +0300

    early version of data sythesis script

--- data/create_data.py
+++ data/create_data.py
@@ -2,17 +2,28 @@ import pandas as pd
 import numpy as np
 
 
+def vibration(temp):
+    return np.log1p(np.abs(temp)) + np.random.normal(0, 0.1)
+
+
 def generate_synthetic_data(n: int = 1000) -> pd.DataFrame:
 
     sensor_ids = [f"Sensor_{i:04d}" for i in range(1, n + 1)]
     materials = ['concrete'] * n
 
+    days = pd.date_range(start='2024-01-01', end='2024-12-31', freq='H')
+    t = np.arange(len(days))
+    temperature = 10 + 15 * np.sin(2 * np.pi * t / (24 * 365)) + np.random.normal(0, 5, len(t))
+    
+    humidity = 100 - (temperature - 10) * 2 + np.random.normal(0, 5, n)
+    vibrations = [vibration(t) for t in temperature]
+
     data = {
         'Sensor ID': sensor_ids,
         'Material': materials,
-        'Temperature': np.random.normal(loc=20, scale=5, size=n),
-        'Humidity': np.random.beta(a=2, b=5, size=n) * 100, 
-        'Vibration': np.random.lognormal(mean=0.5, sigma=0.3, size=n),
+        'Temperature': temperature,
+        'Humidity': humidity,
+        'Vibration': vibrations,
         'Crack Width':np.random.lognormal(mean=-3, sigma=0.3, size=n), 
         'Material Deformation': np.random.normal(loc=0.01, scale=0.002, size=n)
     }
