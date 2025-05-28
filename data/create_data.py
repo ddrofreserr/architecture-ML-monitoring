6462d0f0fd9cecf2352d1fe2cc80c9a98422a569
ddrofreserr <ddrofreserr@gmail.com>
2025-05-28 21:58:26 +0300

    simple data generation added

--- data/create_data.py
+++ data/create_data.py
@@ -2,10 +2,23 @@ import pandas as pd
 import numpy as np
 
 
-def generate_synthetic_data(n_samples: int = 1000) -> pd.DataFrame:
-    return pd.DataFrame({
-        "temperature": np.random.rand(n_samples),
-        "humidity": np.random.rand(n_samples),
-        "target": np.random.randint(0, 2, n_samples)
-    })
-    # добавить шумы
+def generate_synthetic_data(n: int = 1000) -> pd.DataFrame:
+
+    sensor_ids = [f"Sensor_{i:04d}" for i in range(1, n + 1)]
+    materials = ['concrete'] * n
+
+    data = {
+        'Sensor ID': sensor_ids,
+        'Material': materials,
+        'Temperature': np.random.normal(loc=20, scale=5, size=n),
+        'Humidity': np.random.beta(a=2, b=5, size=n) * 100, 
+        'Vibration': np.random.lognormal(mean=0.5, sigma=0.3, size=n),
+        'Crack Width':np.random.lognormal(mean=-3, sigma=0.3, size=n), 
+        'Material Deformation': np.random.normal(loc=0.01, scale=0.002, size=n)
+    }
+
+    noise_std = 0.01
+    for key in data:
+        data[key] += np.random.normal(0, noise_std, size=n)
+
+    return pd.DataFrame(data)
